472502cce404711871eeaa85a7074613586cba63
Roger Herbert <rogerlherbert@gmail.com>
2012-08-27 19:03:09 +0100

    Wrong controller name, auto-load forum model

--- application/controllers/forum.php
+++ application/controllers/forum.php
@@ -3,18 +3,18 @@
 /**
 * 
 */
-class Home extends CI_Controller
+class Forum extends CI_Controller
 {
 	function __construct() 
 	{
 		parent::__construct();
 	
+		$this->load->model('Forum_model');
 		$this->output->enable_profiler(TRUE);
 	}
 
 	public function index()
 	{
-		$this->load->model('Forum_model');
 		$data['topics'] = $this->Forum_model->getRecentTopics();
 
 		$this->load->view('home/index', $data);
