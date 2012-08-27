e5cc0f9981ef12f659c6410c39c73cbb97fd6667
Roger Herbert <rogerlherbert@gmail.com>
2012-08-27 19:31:34 +0100

    just username for now

--- application/views/user/sign_in.php
+++ application/views/user/sign_in.php
@@ -2,7 +2,7 @@
 <?php echo form_open(); ?>
 
 	<div id="username">
-		<?php echo form_label('username or email', 'username'); ?>
+		<?php echo form_label('username', 'username'); ?>
 		<?php echo form_input('username', set_value('username')); ?>
 		<?php echo form_error('username','<span class="form_error">','</span>'); ?>
 	</div>
