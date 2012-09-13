d9df13c0b8a077f786c2a0027eb0aaf78f884f5b
Roger Herbert <rogerlherbert@gmail.com>
2012-09-13 20:42:36 +0100

    home link in footer

--- application/views/common/footer.php
+++ application/views/common/footer.php
@@ -1,10 +1,12 @@
-<?php
+<p><?php
 	if ($this->session->userdata('user_id')) {
-		echo "logged in as ". $this->session->userdata('user_id'). anchor('user/sign_out', "sign out now");
+		echo "logged in as ". anchor('user/id/'.$this->session->userdata('user_id'), $this->session->userdata('user_id')) . " " . anchor('user/sign_out', "sign out now");
 	} else {
 		echo "not logged in. ". anchor('user/sign_in', "sign in now") ." or ". anchor('user/register', "register a new user");
 	}
-?>
+?></p>
+
+<p><?php echo anchor('/', 'Home'); ?></p>
 
 </body>
 </html>
