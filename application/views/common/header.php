250347838a10c5fa4c150ae506c2ae70a1a04ec6
Roger Herbert <rogerlherbert@gmail.com>
2012-09-13 20:24:24 +0100

    little bit of header/boilerplate

--- application/views/common/header.php
+++ application/views/common/header.php
@@ -1,8 +1,15 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
 	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
 	<title>ssdfsdf</title>
-	
+
+	<?php echo link_tag('assets/css/global.css'); ?>
+
+	<script src="<?php echo base_url('assets/js/global.js'); ?>" type="text/javascript"></script>
 </head>
+
 <body>
