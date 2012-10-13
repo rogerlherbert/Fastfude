d849b8ea38dd456e0a1b9f5cf37a85d2f95cb2f2
Roger Herbert <rogerlherbert@gmail.com>
2012-10-13 16:17:44 +0100

    more messaging tweaks

--- application/views/message/index.php
+++ application/views/message/index.php
@@ -11,8 +11,7 @@
 	<?php $class = ($msg['is_read'] == 0) ? ' class="unread"' : ''; ?>
 
 	<li<?php echo $class; ?>>
-		<?php echo img(array('src' => 'assets/img/avatar_48.png', 'data-gravatar' => $msg['gravatar_id'], 'alt' => html_escape($msg['username']), 'class' => 'avatar small')); ?>
-		<?php echo anchor('message/with/'.$user_id, html_escape($msg['username'])); ?>
+		<?php echo anchor('message/with/'.$user_id, img(array('src' => 'http://www.gravatar.com/avatar/'.$msg['gravatar_id'].'?s=48', 'class' => 'avatar')) . html_escape($msg['username'])); ?>
 		<span class="meta"><?php echo timespan($msg['post_time'], time(), 2); ?></span>
 	</li>
 
