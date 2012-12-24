085852fc5582dd757776445dccf175475c4fd3ed
Roger Herbert <rogerlherbert@gmail.com>
2012-12-24 16:09:43 +0000

    config reset

--- application/config/development/config.php
+++ application/config/development/config.php
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '02gYi62QaeJynLTdK8F5';
+$config['encryption_key'] = '';
 
 /*
 |--------------------------------------------------------------------------
