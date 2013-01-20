8824c5e628ac8e3f75f7b2b14f0272d546464475
Roger Herbert <rogerlherbert@gmail.com>
2013-01-20 15:02:08 +0000

    wee installer to set up the db schema in dev

---
+++ application/views/install/index.php
@@ -0,0 +1,6 @@
+<h1>Install</h1>
+<p>This will write a set of empty database tables, removing any old copies of them and their data.</p>
+
+<?php echo form_open(); ?>
+<?php echo form_submit('submit', 'Install'); ?>
+<?php echo form_close(); ?>
