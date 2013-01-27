37dc2524cba52216ff875e0c6df708450a8eb02f
Roger Herbert <rogerlherbert@gmail.com>
2013-01-27 11:15:29 +0000

    installer tweaks

--- application/views/install/index.php
+++ application/views/install/index.php
@@ -1,5 +1,6 @@
 <h1>Install</h1>
-<p>This will write a set of empty database tables, removing any old copies of them and their data.</p>
+<p>You'll need an empty database set up in /application/config/development/database.php</p>
+<p>This installer will write a set of empty tables to it, overwriting any old copies of them and erasing their data.</p>
 
 <?php echo form_open(); ?>
 <?php echo form_submit('submit', 'Install'); ?>
