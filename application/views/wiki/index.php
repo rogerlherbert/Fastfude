5b0afab6130c1f366d2c98b6b89145b9bb3f0862
Roger Herbert <rogerlherbert@gmail.com>
2013-04-06 16:02:30 +0100

    recent wiki edits markup

--- application/views/wiki/index.php
+++ application/views/wiki/index.php
@@ -2,24 +2,20 @@
 
 <div class="span3">
 	<div class="well sidebar-nav">
-
+		<?php echo anchor('wiki/create', '<i class="icon-plus"></i> Create a wiki page', ' class="btn"'); ?>	
 	</div>
 </div>
 
 <div class="span9">
 
-<div class="btn-group">
-	<?php echo anchor('wiki/create', '<i class="icon-plus"></i> Create a wiki page', ' class="btn"'); ?>	
-</div>
-
-<ol class="edit_history unstyled">
+<ol class="edit-history unstyled">
 <?php foreach ($history as $edit) { ?>
 	<li>
-		<p class="wiki_title"><?php echo anchor('wiki/page/'.$edit->stub.'/'.$edit->edit_id, '#'.$edit->edit_id . ': ' . $edit->title); ?></p>
-		<p class="meta">
+		<h3><?php echo anchor('wiki/page/'.$edit->stub.'/'.$edit->edit_id, '#'.$edit->edit_id . ': ' . $edit->title); ?></h3>
+		<p><small>
 			<span>Edited by <?php echo anchor('user/id/'.$edit->user_id, $edit->username); ?></span>
-			<span><?php echo timespan($edit->created); ?> ago</span>
-		</p>
+			<span><?php echo timespan($edit->created, time(), 2); ?> ago</span>
+		</small></p>
 	</li>
 <?php } ?>
 </ol>
