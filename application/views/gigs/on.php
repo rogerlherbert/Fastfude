501e7b09aa6e7bf37ad5d57a431d29815f93e232
Roger Herbert <rogerlherbert@gmail.com>
2013-05-06 12:17:54 +0100

    standardise gig list markup

--- application/views/gigs/on.php
+++ application/views/gigs/on.php
@@ -4,27 +4,23 @@
 
 <?php if (isset($gigs)) { ?>
 	<?php if (count($gigs) > 0) { ?>
-		<ol class="vcalendar unstyled">
+		<ol class="vcalendar unstyled media">
 
-		<?php foreach ($gigs as $gig) { ?>
-			<li class="vevent">
-				<?php if ($gig->topic_id != '') { ?>
-					<div><?php echo anchor('topic/id/'.$gig->topic_id, $gig->gig_title, 'class="url"'); ?></div>
-				<?php } elseif ($gig->gig_title != '') { ?>
-					<div><?php echo $gig->gig_title; ?></div>
-				<?php } ?>
-			
+			<?php foreach ($gigs as $gig) { ?>
+			<li class="vevent media-body">
 				<?php if ($gig->lineup != '') { ?>
-					<p class="meta summary"><?php echo $gig->lineup; ?></p>
+					<h3 class="summary media-title"><?php echo $gig->lineup; ?></h3>
+				<?php } else { ?>
+					<h3 class="summary media-title"><?php echo $gig->title; ?></h3>
 				<?php } ?>
 				
-				<p class="meta">
-					<?php if ($gig->topic_id != '') { echo $gig->replies . " replies | "; } ?>
+				<p><small>
+					<?php if ($gig->topic_id != '') { echo anchor('topic/id/'.$gig->topic_id, $gig->replies . " replies", 'class="url"') . " | "; } ?>
 					<span class="location"><?php echo $gig->location; ?></span>
 					Doors at <time class="dtstart" datetime="<?php echo date('c', $gig->start_time); ?>"><?php echo date('h:i a', $gig->start_time); ?></time>
-				</p>
+				</small></p>
 			</li>
-		<?php } ?>
+			<?php } ?>
 
 		</ol>
 	<?php } else { ?>
